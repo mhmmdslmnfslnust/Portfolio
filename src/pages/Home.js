2c3a6403b976faf5e19fcb07350e22dd6fb37498
Muhammad Suleman Faisal <mfaisal.bese23seecs@student.nust.edu.pk>
2025-05-18 04:14:14 +0500

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,7 +12,8 @@ const Home = () => {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      transition={{ duration: 0.5 }}
+      // transition={{ duration: 0.5 }}
+      transition={{ duration: 2, ease: 'easeInOut' }}
     >
       <Hero />
       <About />
