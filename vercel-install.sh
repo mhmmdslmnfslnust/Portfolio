ac22e5ed2f9d73a9444ecb781088d92277936bbe
Muhammad Suleman Faisal <mfaisal.bese23seecs@student.nust.edu.pk>
2026-07-17 16:30:25 +0500

    revert

--- vercel-install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ "$VERCEL_GIT_COMMIT_REF" == "wheel-background" ]; then
-  echo "Simple branch: Skipping dependency installation."
-  exit 0
-else
-  echo "Main branch: Installing dependencies."
-  npm install # Change to yarn install or pnpm install if your main branch uses them
-fi
