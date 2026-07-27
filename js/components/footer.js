1aff4c11f5108ed9efa1e39305b3ebc0d342db99
Muhammad Suleman Faisal <mfaisal.bese23seecs@student.nust.edu.pk>
2026-07-27 10:34:58 +0500

    complete new file structure

---
+++ js/components/footer.js
@@ -0,0 +1,5 @@
+/*
+      ============================================================
+      FOOTER YEAR — auto-updates so you never have to touch it
+    */
+document.getElementById("footer-year").textContent = new Date().getFullYear();
---
+++ js/main.js
@@ -0,0 +1,6 @@
+import "./components/contact-form.js"
+import "./components/fan.js";
+import "./components/footer.js";
+import "./components/project-filter.js";
+import "./components/rotation.js";
+import "./components/wheel.js";
